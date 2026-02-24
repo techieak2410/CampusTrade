abaf616ad686e183f7b36954a758c2e027a60801
techieak2410 <akgupta241004@gmail.com>
2026-02-24 08:46:56 +0530

    migrated the backend to  a new backend folder for better understanding

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,6 +2,10 @@ import dotenv from 'dotenv'
 dotenv.config();
 import mongoose from 'mongoose';
 
+//this was added becaause there is some problme with node 24+ versions and dns error occurs
+import dns from 'dns'
+dns.setServers(['8.8.8.8']);
+
 async function connectDb(){
     try{
         await mongoose.connect(process.env.MONGO_URI);
